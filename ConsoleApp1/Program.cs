03af6b5d2f9ab82976c0375957e863b28aec3390
himajaGandham <42212517+himajaGandham@users.noreply.github.com>
2019-06-01 18:22:17 +0100

    cm2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello This is the first Program master edit test master");
+            Console.WriteLine("Hello This is the first Program master edit test master1");
             Console.ReadLine();
         }
     }
